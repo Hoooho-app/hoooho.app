a60b2bcd7c6f6aa249285a161c83d088a8b622c6
Hoooho-app <310485636+Hoooho-app@users.noreply.github.com>
2026-09-11 13:51:06 +0800

    Refine sidebar service icon

--- src/components/navigation/NurseCapIcon.tsx
+++
@@ -1,7 +0,0 @@
-import { createLucideIcon } from 'lucide-react'
-
-export const NurseCapIcon = createLucideIcon('NurseCap', [
-  ['path', { d: 'M5 15.5V9.8a2 2 0 0 1 1.4-1.9L12 6l5.6 1.9A2 2 0 0 1 19 9.8v5.7', key: 'cap' }],
-  ['path', { d: 'M4 15.5h16v2a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2z', key: 'band' }],
-  ['path', { d: 'M12 9v4M10 11h4', key: 'cross' }]
-])
